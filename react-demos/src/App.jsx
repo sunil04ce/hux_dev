278a7d84f222d73256ea33eeaae42e6ea30abe98
sunil parmar <sunil04ce@gmail.com>
2025-03-22 19:34:06 +0530

    intial code

---
+++ react-demos/src/App.jsx
@@ -0,0 +1,12 @@
+import Greet from "./components/Greeting";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Greet />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-demos/src/components/Greeting.jsx
@@ -0,0 +1,5 @@
+const Greet = () => {
+  return <h1>Welcome to React practice!</h1>;
+};
+
+export default Greet;
